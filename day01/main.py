e273111d8f8a10041573044d39ed1a4ccfcde33c
Floozutter <me@floozutter.site>
2024-01-02 14:40:39 -0800

    add day01 part 1

--- day01/main.py
+++ day01/main.py
@@ -2,3 +2,9 @@ INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+lines = raw.strip().split()
+
+def calibration(s: str) -> int:
+    digits = "".join(c for c in s if c.isdigit())
+    return 10*int(digits[0]) + int(digits[-1])
+print(sum(map(calibration, lines)))
