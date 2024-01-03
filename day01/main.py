8a949f8b0a0295a504fd980bed5a5f38bf7840ac
Floozutter <me@floozutter.site>
2024-01-02 21:49:16 -0800

    clean day01 by reversing on part 2

--- day01/main.py
+++ day01/main.py
@@ -15,13 +15,14 @@ digit_values = (
     {str(i):i for i in range(1, 10)} |
     {"one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9}
 )
-digit_pattern = "|".join(digit_values.keys())
+tigid_values = {k[::-1]: v for k, v in digit_values.items()}
+digit_pattern = re.compile("|".join(digit_values.keys()))
+tigid_pattern = re.compile("|".join(tigid_values.keys()))
 def part2(s: str) -> int:
-    # re.findall can fail to find last because it searches for non-overlapping matches only
-    first = re.search(digit_pattern, s)
-    last = re.search(f"(?s:.*)({digit_pattern})", s)  # https://stackoverflow.com/a/33233868
+    first = digit_pattern.search(s)
+    last = tigid_pattern.search(s[::-1])
     assert first is not None and last is not None
-    return 10*digit_values[first[0]] + digit_values[last[1]]
+    return 10*digit_values[first[0]] + tigid_values[last[0]]
 
 print(sum(map(part1, lines)))
 print(sum(map(part2, lines)))
