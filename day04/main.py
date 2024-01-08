edfb759bb4b8d369acd57255042d90feb74a8690
Floozutter <me@floozutter.site>
2024-01-08 12:46:11 -0800

    add day04 part 1

--- day04/main.py
+++ day04/main.py
@@ -1,4 +1,19 @@
+from typing import NamedTuple, Self
+
+class Card(NamedTuple):
+    id: int
+    winners: frozenset[int]
+    numbers: frozenset[int]
+    @classmethod
+    def from_line(cls, line: str) -> Self:
+        head, tail = line.split(": ")
+        l, r = tail.split(" | ")
+        return cls(int(head[5:]), frozenset(map(int, l.split())), frozenset(map(int, r.split())))
+
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+cards = tuple(map(Card.from_line, raw.strip().splitlines()))
+
+print(sum(int(2 ** (len(c.winners & c.numbers) - 1)) for c in cards))
