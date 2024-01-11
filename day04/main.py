b485ab89ed0ad6eb6a271add75999343981517d7
Floozutter <me@floozutter.site>
2024-01-10 23:13:35 -0800

    multiply [1] instead of list comprehension

--- day04/main.py
+++ day04/main.py
@@ -9,7 +9,7 @@ wincounts = tuple(
 )
 print(sum(int(2**(n-1)) for n in wincounts))
 
-scratchcards = [1 for i in range(len(wincounts))]
+scratchcards = [1] * len(wincounts)
 for i, n in enumerate(wincounts):
     for j in range(i+1, i+1+n):
         scratchcards[j] += scratchcards[i]
