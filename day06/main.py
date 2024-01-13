b8dcce3eb92ec820ceca07ec943ed8ced190869c
Floozutter <me@floozutter.site>
2024-01-12 21:53:33 -0800

    add day06 part 2

--- day06/main.py
+++ day06/main.py
@@ -6,3 +6,6 @@ with open(INPUTPATH) as ifile:
     raw = ifile.read()
 times, records = (tuple(map(int, line.split()[1:])) for line in raw.strip().splitlines())
 print(prod(sum(1 for i in range(t) if i*(t-i) > r) for t, r in zip(times, records)))
+
+time, record = int("".join(map(str, times))), int("".join(map(str, records)))
+print(sum(1 for i in range(time) if i*(time-i) > record))
