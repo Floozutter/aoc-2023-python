211d5b45b86a238dc7293407ad3a3583b3ce7a4a
Floozutter <me@floozutter.site>
2024-01-12 21:52:27 -0800

    add day06 part 1

--- day06/main.py
+++ day06/main.py
@@ -1,4 +1,8 @@
+from math import prod
+
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+times, records = (tuple(map(int, line.split()[1:])) for line in raw.strip().splitlines())
+print(prod(sum(1 for i in range(t) if i*(t-i) > r) for t, r in zip(times, records)))
