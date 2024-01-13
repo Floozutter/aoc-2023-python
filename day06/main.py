2f015b3719a3021fcc27a185a1a0e2c786b33bd5
Floozutter <me@floozutter.site>
2024-01-12 22:45:03 -0800

    clean day06 using the quadratic formula
    use the quadratic formula to find the exclusive lower and upper bounds
    for how long you can hold the button and get a distance higher than the
    record

--- day06/main.py
+++ day06/main.py
@@ -1,11 +1,16 @@
-from math import prod
+from math import ceil, floor, prod, sqrt
+
+def winners(time: int, record: int) -> int:
+    lower = (time-sqrt(time**2-4*record))/2
+    upper = (time+sqrt(time**2-4*record))/2
+    return ceil(upper) - floor(lower) - 1
 
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
 times, records = (tuple(map(int, line.split()[1:])) for line in raw.strip().splitlines())
-print(prod(sum(1 for i in range(t) if i*(t-i) > r) for t, r in zip(times, records)))
+print(prod(winners(t, r) for t, r in zip(times, records)))
 
 time, record = int("".join(map(str, times))), int("".join(map(str, records)))
-print(sum(1 for i in range(time) if i*(time-i) > record))
+print(winners(time, record))
