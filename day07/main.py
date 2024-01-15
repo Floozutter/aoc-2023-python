ef6f70aca30dc2e31047f89a8ae1695e84a1e0dc
Floozutter <me@floozutter.site>
2024-01-14 21:51:55 -0800

    parse day07 input

--- day07/main.py
+++ day07/main.py
@@ -2,3 +2,4 @@ INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+pairs = tuple((l, int(r)) for l, r in map(str.split, raw.strip().splitlines()))
