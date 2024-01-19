e162dec7e77e42e8a3401757057794ad973a7704
Floozutter <me@floozutter.site>
2024-01-19 11:41:56 -0800

    parse day08 input

--- day08/main.py
+++ day08/main.py
@@ -2,3 +2,5 @@ INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+instructions, tail = raw.strip().split("\n\n")
+network = {l: (r[1:4], r[-4:-1]) for l, r in (line.split(" = ") for line in tail.splitlines())}
