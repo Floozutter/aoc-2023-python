7f52ed8328b666f5f1d79703d04030ad84e4d660
Floozutter <me@floozutter.site>
2024-01-20 09:07:48 -0800

    add day08 part 1

--- day08/main.py
+++ day08/main.py
@@ -4,3 +4,10 @@ with open(INPUTPATH) as ifile:
     raw = ifile.read()
 instructions, tail = raw.strip().split("\n\n")
 network = {l: (r[1:4], r[-4:-1]) for l, r in (line.split(" = ") for line in tail.splitlines())}
+
+steps = 0
+node = "AAA"
+while node != "ZZZ":
+    node = network[node][0 if instructions[steps % len(instructions)] == "L" else 1]
+    steps += 1
+print(steps)
