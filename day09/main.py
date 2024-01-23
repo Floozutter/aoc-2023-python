f258172e2f45a7c3508776d47889d3e1a353c31d
Floozutter <me@floozutter.site>
2024-01-23 08:07:24 -0800

    parse day09 input

--- day09/main.py
+++ day09/main.py
@@ -2,3 +2,4 @@ INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+histories = tuple(tuple(map(int, line.split())) for line in raw.strip().splitlines())
