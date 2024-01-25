28f397a397d90a7cb98cf9a02d0680a0b35523c7
Floozutter <me@floozutter.site>
2024-01-25 01:00:04 -0800

    add day09 part 2

--- day09/main.py
+++ day09/main.py
@@ -1,8 +1,8 @@
-def predict(history: tuple[int, ...]) -> int:
+def predict(history: tuple[int, ...]) -> tuple[int, int]:
     if all(z == 0 for z in history):
-        return 0
-    diffs = tuple(b - a for a, b in zip(history, history[1:]))
-    return history[-1] + predict(diffs)
+        return 0, 0
+    ldiff, rdiff = predict(tuple(b - a for a, b in zip(history, history[1:])))
+    return history[0] - ldiff, history[-1] + rdiff
 
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
@@ -10,4 +10,6 @@ with open(INPUTPATH) as ifile:
     raw = ifile.read()
 histories = tuple(tuple(map(int, line.split())) for line in raw.strip().splitlines())
 
-print(sum(map(predict, histories)))
+lsum, rsum = map(sum, zip(*map(predict, histories)))
+print(rsum)
+print(lsum)
