12784dd72a7c8942674889d84ed81d1104d1d8a7
Floozutter <me@floozutter.site>
2024-01-27 03:53:29 -0800

    parse day10 input

--- day10/main.py
+++ day10/main.py
@@ -1,4 +1,5 @@
 INPUTPATH = "input.txt"
-#INPUTPATH = "input-test.txt"
+INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+tiles = {(i, j): c for i, line in enumerate(raw.strip().splitlines()) for j, c in enumerate(line)}
