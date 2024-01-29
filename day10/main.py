687eb423065f0b590d2dbffddcc53f3b30c118b1
Floozutter <me@floozutter.site>
2024-01-29 03:49:16 -0800

    add day10 part 2

--- day10/main.py
+++ day10/main.py
@@ -1,10 +1,16 @@
+ADJACENTS = ((-1, 0), (0, -1), (0, 1), (1, 0))
+
 INPUTPATH = "input.txt"
-#INPUTPATH = "input-test.txt"
+#INPUTPATH = "input-test1.txt"
+#INPUTPATH = "input-test2.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+tiles = {(a, b): t for a, line in enumerate(raw.strip().splitlines()) for b, t in enumerate(line)}
+
+si, sj = next((3*a + 1, 3*b + 1) for (a, b), t in tiles.items() if t == "S")
 subtiles = {
     (3*a + di, 3*b + dj): st
-    for a, line in enumerate(raw.strip().splitlines()) for b, t in enumerate(line)
+    for (a, b), t in tiles.items()
     for di, subtilerow in enumerate(
         ((0, 1, 0), (0, 1, 0), (0, 1, 0)) if t == "|" else
         ((0, 0, 0), (1, 1, 1), (0, 0, 0)) if t == "-" else
@@ -12,38 +18,39 @@ subtiles = {
         ((0, 1, 0), (1, 1, 0), (0, 0, 0)) if t == "J" else
         ((0, 0, 0), (1, 1, 0), (0, 1, 0)) if t == "7" else
         ((0, 0, 0), (0, 1, 1), (0, 1, 0)) if t == "F" else
-        ((0, 0, 0), (0, 2, 0), (0, 0, 0)) if t == "S" else
+        ((0, 0, 0), (0, 1, 0), (0, 0, 0)) if t == "S" else
         ((0, 0, 0), (0, 0, 0), (0, 0, 0))
     )
     for dj, st in enumerate(subtilerow)
 }
-
-si, sj = next(p for p, st in subtiles.items() if st == 2)
-for di, dj in ((-1, 0), (0, -1), (0, 1), (1, 0)):
+for di, dj in ADJACENTS:
     subtiles[si + di, sj + dj] = subtiles.get((si + 2*di, sj + 2*dj), 0)
 
-visited = {(si, sj)}
-frontier = frozenset(((si, sj),))
-while (frontier := frozenset(
-    (i + di, j + dj)
-    for i, j in frontier for di, dj in ((-1, 0), (0, -1), (0, 1), (1, 0))
-    if subtiles.get((i + di, j + dj), 0) == 1
-)):
-    subtiles.update({p: 2 for p in frontier})
-    visited.update(frontier)
-print(len(visited)//6)
+def contigous_subtiles(pos: tuple[int, int]) -> set[tuple[int, int]]:
+    contiguous = {pos}
+    frontier = frozenset(contiguous)
+    while (frontier := frozenset(
+        (i + di, j + dj)
+        for i, j in frontier for di, dj in ADJACENTS
+        if subtiles.get((i + di, j + dj)) == subtiles[pos] and (i + di, j + dj) not in contiguous
+    )):
+        contiguous |= frontier
+    return contiguous
 
-cache: dict[tuple[int, int], bool] = {}
-def enclosed(i: int, j: int, visited: frozenset[tuple[int, int]] = frozenset()) -> bool:
-    if (i, j) not in cache:
-        cache[i, j] = (i, j) in subtiles and subtiles[i, j] == 0 and all(
-            subtiles.get((i + di, j + dj), 0) == 2 or enclosed(i + di, j + dj, frozenset((*visited, (i, j))))
-            for di, dj in ((-1, 0), (0, -1), (0, 1), (1, 0))
-            if (i + di, j + dj) not in visited
-        )
-    return cache[i, j]
+pipeloop = contigous_subtiles((si, sj))
+print(len(pipeloop) // 6)
+
+subtiles.update({pos: 0 for pos in subtiles if pos not in pipeloop})
+
+enclosed: set[tuple[int, int]] = set()
+unchecked = {pos for pos, st in subtiles.items() if st == 0}
+while unchecked:
+    region = contigous_subtiles(unchecked.pop())
+    if all((i + di, j + dj) in subtiles for i, j in region for di, dj in ADJACENTS):
+        enclosed |= region
+    unchecked -= region
 
 print(sum(
-    1 for a in range(max(subtiles)[0]//3) for b in range(max(subtiles)[1]//3)
-    if all(enclosed(3*a + di, 3*b + dj) for di in range(3) for dj in range(3))
+    1 for a, b in tiles
+    if all((3*a + di, 3*b + dj) in enclosed for di in range(3) for dj in range(3))
 ))
