a258c2ada635a8bc64d9b47af0cc2032883fa375
Floozutter <me@floozutter.site>
2024-01-29 01:18:34 -0800

    add day10 part 2 recursive attempt (blows stack)

--- day10/main.py
+++ day10/main.py
@@ -4,15 +4,15 @@ with open(INPUTPATH) as ifile:
     raw = ifile.read()
 subtiles = {
     (3*a + di, 3*b + dj): st
-    for a, line in enumerate(raw.strip().splitlines()) for b, c in enumerate(line)
+    for a, line in enumerate(raw.strip().splitlines()) for b, t in enumerate(line)
     for di, subtilerow in enumerate(
-        ((0, 1, 0), (0, 1, 0), (0, 1, 0)) if c == "|" else
-        ((0, 0, 0), (1, 1, 1), (0, 0, 0)) if c == "-" else
-        ((0, 1, 0), (0, 1, 1), (0, 0, 0)) if c == "L" else
-        ((0, 1, 0), (1, 1, 0), (0, 0, 0)) if c == "J" else
-        ((0, 0, 0), (1, 1, 0), (0, 1, 0)) if c == "7" else
-        ((0, 0, 0), (0, 1, 1), (0, 1, 0)) if c == "F" else
-        ((0, 0, 0), (0, 2, 0), (0, 0, 0)) if c == "S" else
+        ((0, 1, 0), (0, 1, 0), (0, 1, 0)) if t == "|" else
+        ((0, 0, 0), (1, 1, 1), (0, 0, 0)) if t == "-" else
+        ((0, 1, 0), (0, 1, 1), (0, 0, 0)) if t == "L" else
+        ((0, 1, 0), (1, 1, 0), (0, 0, 0)) if t == "J" else
+        ((0, 0, 0), (1, 1, 0), (0, 1, 0)) if t == "7" else
+        ((0, 0, 0), (0, 1, 1), (0, 1, 0)) if t == "F" else
+        ((0, 0, 0), (0, 2, 0), (0, 0, 0)) if t == "S" else
         ((0, 0, 0), (0, 0, 0), (0, 0, 0))
     )
     for dj, st in enumerate(subtilerow)
@@ -32,3 +32,18 @@ while (frontier := frozenset(
     subtiles.update({p: 2 for p in frontier})
     visited.update(frontier)
 print(len(visited)//6)
+
+cache: dict[tuple[int, int], bool] = {}
+def enclosed(i: int, j: int, visited: frozenset[tuple[int, int]] = frozenset()) -> bool:
+    if (i, j) not in cache:
+        cache[i, j] = (i, j) in subtiles and subtiles[i, j] == 0 and all(
+            subtiles.get((i + di, j + dj), 0) == 2 or enclosed(i + di, j + dj, frozenset((*visited, (i, j))))
+            for di, dj in ((-1, 0), (0, -1), (0, 1), (1, 0))
+            if (i + di, j + dj) not in visited
+        )
+    return cache[i, j]
+
+print(sum(
+    1 for a in range(max(subtiles)[0]//3) for b in range(max(subtiles)[1]//3)
+    if all(enclosed(3*a + di, 3*b + dj) for di in range(3) for dj in range(3))
+))
