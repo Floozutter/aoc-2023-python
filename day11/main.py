3420760e636a4d83cdf8bda18cf7881dcd2b453d
Floozutter <me@floozutter.site>
2024-01-31 15:28:30 -0800

    parse day11 input

--- day11/main.py
+++ day11/main.py
@@ -2,3 +2,4 @@ INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+galaxies = {(i, j) for i, line in raw.strip().splitlines() for j, c in line if c == "#"}
