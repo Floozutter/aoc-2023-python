dffe306656504d16309ba51b11feff6caec23fe0
Floozutter <me@floozutter.site>
2024-02-01 07:10:58 -0800

    add day11 part 2

--- day11/main.py
+++ day11/main.py
@@ -1,16 +1,19 @@
 from itertools import combinations
 
-def expand(galaxies: frozenset[tuple[int, int]]) -> frozenset[tuple[int, int]]:
+def distance(a: tuple[int, int], b: tuple[int, int]) -> int:
+    return abs(b[0] - a[0]) + abs(b[1] - a[1])
+
+def expand(galaxies: frozenset[tuple[int, int]], scale: int) -> frozenset[tuple[int, int]]:
     xs, ys = (list(column) for column in zip(*sorted(galaxies)))
     for i in range(len(xs) - 1):
-        distance = xs[i+1] - xs[i]
+        skipped = max(0, xs[i+1] - xs[i] - 1)
         for j in range(i + 1, len(xs)):
-            xs[j] += max(0, distance - 1)
+            xs[j] += skipped * (scale - 1)
     ys, xs = (list(column) for column in zip(*sorted(zip(ys, xs))))
     for i in range(len(ys) - 1):
-        distance = ys[i+1] - ys[i]
+        skipped = max(0, ys[i+1] - ys[i] - 1)
         for j in range(i + 1, len(ys)):
-            ys[j] += max(0, distance - 1)
+            ys[j] += skipped * (scale - 1)
     return frozenset(zip(xs, ys))
 
 INPUTPATH = "input.txt"
@@ -18,9 +21,8 @@ INPUTPATH = "input.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
 galaxies = frozenset(
-    (x, y)
-    for y, line in enumerate(raw.strip().splitlines()) for x, c in enumerate(line)
-    if c == "#"
+    (x, y) for y, s in enumerate(raw.strip().splitlines()) for x, c in enumerate(s) if c == "#"
 )
 
-print(sum(abs(bx - ax) + abs(by - ay) for (ax, ay), (bx, by) in combinations(expand(galaxies), 2)))
+print(sum(distance(a, b) for a, b in combinations(expand(galaxies, 2), 2)))
+print(sum(distance(a, b) for a, b in combinations(expand(galaxies, 1_000_000), 2)))
