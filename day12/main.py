d817288d9f97f4fcb2d6605be734f10ed98fddb4
Floozutter <me@floozutter.site>
2024-02-03 14:35:31 -0800

    parse day12 input

--- day12/main.py
+++ day12/main.py
@@ -1,4 +1,15 @@
+from typing import NamedTuple, Self
+
+class Record(NamedTuple):
+    pattern: str
+    groups: tuple[int, ...]
+    @classmethod
+    def from_line(cls, line: str) -> Self:
+        pattern, tail = line.split()
+        return cls(pattern, tuple(map(int, tail.split(","))))
+
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+records = tuple(map(Record.from_line, raw.strip().splitlines()))
