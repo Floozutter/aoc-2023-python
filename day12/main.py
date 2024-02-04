f2ed30fe9284182715c56341762cda56cc1a23d1
Floozutter <me@floozutter.site>
2024-02-04 02:15:36 -0800

    add day12 part 1

--- day12/main.py
+++ day12/main.py
@@ -1,4 +1,5 @@
-from typing import NamedTuple, Self
+from itertools import combinations
+from typing import NamedTuple, Self, Iterator
 
 class Record(NamedTuple):
     pattern: str
@@ -7,9 +8,21 @@ class Record(NamedTuple):
     def from_line(cls, line: str) -> Self:
         pattern, tail = line.split()
         return cls(pattern, tuple(map(int, tail.split(","))))
+    def arrangements(self) -> Iterator[str]:
+        unknowns = tuple(i for i, c in enumerate(self.pattern) if c == "?")
+        missing = sum(self.groups) - self.pattern.count("#")
+        for damageds in (frozenset(comb) for comb in combinations(unknowns, missing)):
+            a = "".join(
+                c if c != "?" else "#" if i in damageds else "."
+                for i, c in enumerate(self.pattern)
+            )
+            if tuple(len(g) for g in a.replace(".", " ").strip().split()) == self.groups:
+                yield a
 
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
 records = tuple(map(Record.from_line, raw.strip().splitlines()))
+
+print(sum(1 for r in records for a in r.arrangements()))
