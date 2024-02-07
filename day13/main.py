742368d13c17b29075deaedd8b276ded2306ff4e
Floozutter <me@floozutter.site>
2024-02-07 13:55:39 -0800

    parse day13 input

--- day13/main.py
+++ day13/main.py
@@ -2,3 +2,7 @@ INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+patterns = tuple(
+    {(i, j): c for i, line in enumerate(section.splitlines()) for j, c in enumerate(line)}
+    for section in raw.strip().split("\n\n")
+)
