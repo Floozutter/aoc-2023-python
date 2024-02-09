b8d3c71cde4fc609a9cc38e45487e04809b4f450
Floozutter <me@floozutter.site>
2024-02-08 22:20:02 -0800

    add day 13 part 1 skeleton

--- day13/main.py
+++ day13/main.py
@@ -1,8 +1,17 @@
+from typing import NamedTuple, Self
+
+class Pattern(NamedTuple):
+    rows: tuple[tuple[bool, ...], ...]
+    @classmethod
+    def from_section(cls, section: str) -> Self:
+        return cls(tuple(tuple(c == "#" for c in line) for line in section.splitlines()))
+    def summary(self) -> int:
+        return 0
+
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
-patterns = tuple(
-    {(i, j): c for i, line in enumerate(section.splitlines()) for j, c in enumerate(line)}
-    for section in raw.strip().split("\n\n")
-)
+patterns = tuple(map(Pattern.from_section, raw.strip().split("\n\n")))
+
+print(sum(map(Pattern.summary, patterns)))
