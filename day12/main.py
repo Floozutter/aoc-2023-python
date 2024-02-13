4f4c1b7f7b01e691dcc3e4f4301ef16ee48f8d30
Floozutter <me@floozutter.site>
2024-02-13 10:42:14 -0800

    represent Records as contiguous blocks of springs

--- day12/main.py
+++ day12/main.py
@@ -1,23 +1,28 @@
-from itertools import combinations
-from typing import NamedTuple, Self
+from itertools import groupby
+from typing import Iterable, NamedTuple, Self
 
 class Record(NamedTuple):
-    pattern: str
-    groups: tuple[int, ...]
+    blocks: tuple[int, ...]
+    sizes: tuple[int, ...]
     @classmethod
-    def from_line(cls, line: str) -> Self:
-        pattern, tail = line.split()
-        return cls(pattern, tuple(map(int, tail.split(","))))
-    def arrangements(self) -> int:
-        return 0
+    def from_parts(cls, pattern: str, sizes: Iterable[int]) -> Self:
+        pairs = ((c, sum(1 for _ in group)) for c, group in groupby(pattern))
+        return cls(tuple(n if c == "#" else -n if c == "?" else 0 for c, n in pairs), tuple(sizes))
     def unfold(self) -> Self:
-        return type(self)("?".join(5*(self.pattern,)), 5*self.groups)
+        pattern = "".join("#"*b if b > 0 else "?"*-b if b < 0 else "." for b in self.blocks)
+        return type(self).from_parts("?".join((pattern,)*5), self.sizes*5)
+
+def arrangements(rec: Record) -> int:
+    return 0
 
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
-records = tuple(map(Record.from_line, raw.strip().splitlines()))
+records = tuple(
+    Record.from_parts(pattern, map(int, tail.split(",")))
+    for pattern, tail in map(str.split, raw.strip().splitlines())
+)
 
-print(sum(r.arrangements() for r in records))
-print(sum(r.unfold().arrangements() for r in records))
+print(sum(arrangements(r) for r in records))
+print(sum(arrangements(r.unfold()) for r in records))
