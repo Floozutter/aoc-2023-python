d2b982351fef996eac5256df2ff3c73f8325f34f
Floozutter <me@floozutter.site>
2024-02-17 13:03:16 -0800

    fix wrong closing bracket

--- day14/main.py
+++ day14/main.py
@@ -2,4 +2,4 @@ INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
-platform = {(i, j): c for i, line in enumerate(raw.splitlines() for j, c in enumerate(line)}
+platform = {(i, j): c for i, line in enumerate(raw.splitlines()) for j, c in enumerate(line)}
