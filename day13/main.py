38a02e360e2b2d09514c840db8cd27ff470d8451
Floozutter <me@floozutter.site>
2024-02-19 10:56:53 -0800

    rewrite day13 skeleton and add summarize

--- day13/main.py
+++ day13/main.py
@@ -1,17 +1,26 @@
 from typing import NamedTuple, Self
 
-class Pattern(NamedTuple):
-    rows: tuple[tuple[bool, ...], ...]
-    @classmethod
-    def from_section(cls, section: str) -> Self:
-        return cls(tuple(tuple(c == "#" for c in line) for line in section.splitlines()))
-    def summary(self) -> int:
-        return 0
+def horizontal_reflection(rows: tuple[tuple[bool, ...], ...]) -> None | int:
+    for i in range(len(rows) - 1):
+        if False:
+            return i
+    return None
+
+def summarize(rows: tuple[tuple[bool, ...], ...]) -> int:
+    if (horizontal := horizontal_reflection(rows)) is not None:
+        return 100 * (horizontal + 1)
+    transposed = tuple(tuple(column) for column in zip(*rows))
+    if (vertical := horizontal_reflection(transposed)) is not None:
+        return vertical + 1
+    raise ValueError
 
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
-patterns = tuple(map(Pattern.from_section, raw.strip().split("\n\n")))
+patterns = tuple(
+    tuple(tuple(c == "#" for c in line) for line in section.splitlines())
+    for section in raw.strip().split("\n\n")
+)
 
-print(sum(map(Pattern.summary, patterns)))
+print(sum(map(summarize, patterns)))
