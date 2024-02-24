3a0be621fb23e2dd7d5f21d85bc3d0b84af53e28
Floozutter <me@floozutter.site>
2024-02-23 16:19:36 -0800

    add day14 part 2

--- day14/main.py
+++ day14/main.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def tilt_left(s: str) -> str:
     cubes = (-1, *(i for i, c in enumerate(s) if c == "#"), len(s))
     return "".join(
@@ -26,3 +28,12 @@ with open(INPUTPATH) as ifile:
 platform = tuple(raw.strip().splitlines())
 
 print(load(tilt(platform, 3), 3))
+
+visits: dict[tuple[str, ...], list[int]] = defaultdict(list)
+p = platform
+for i in range(1_000_000_000):
+    visits[p].append(i)
+    if len(visits[p]) >= 2 and (1_000_000_000-visits[p][-2]) % (visits[p][-1]-visits[p][-2]) == 0:
+        break
+    p = tilt(tilt(tilt(tilt(p, 3), 0), 1), 2)
+print(load(p, 3))
