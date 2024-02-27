0587360ffb8ee47318e7e7b8450b50022b472aad
Floozutter <me@floozutter.site>
2024-02-26 20:10:09 -0800

    add day15 part 2

--- day15/main.py
+++ day15/main.py
@@ -1,8 +1,5 @@
-def hashbrown(s: str) -> int:
-    value = 0
-    for c in s:
-        value = (value + ord(c)) * 17 % 256
-    return value
+def hashbrown(s: str, value: int = 0) -> int:
+    return value if not s else hashbrown(s[1:], (value + ord(s[0])) * 17 % 256)
 
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
@@ -11,3 +8,20 @@ with open(INPUTPATH) as ifile:
 steps = tuple(raw.strip().split(","))
 
 print(sum(map(hashbrown, steps)))
+
+boxes: tuple[list[tuple[str, int]], ...] = tuple([] for _ in range(256))
+for s in steps:
+    opi = next(i for i, c in enumerate(s) if not c.isalpha())
+    label = s[:opi]
+    box = boxes[hashbrown(label)]
+    sloti = next((i for i, (l, _) in enumerate(box) if l == label), None)
+    if s[opi] == "-":
+        if sloti is not None:
+            box.pop(sloti)
+    else:
+        focal = int(s[opi+1:])
+        if sloti is not None:
+            box[sloti] = (label, focal)
+        else:
+            box.append((label, focal))
+print(sum((i+1)*(j+1)*f for i, box in enumerate(boxes) for j, (_, f) in enumerate(box)))
