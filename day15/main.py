6bc187ce0a1bc9f478ad002bc54ebd4ed5a4a596
Floozutter <me@floozutter.site>
2024-02-26 17:52:33 -0800

    add day15 part 1

--- day15/main.py
+++ day15/main.py
@@ -1,4 +1,13 @@
+def hashbrown(s: str) -> int:
+    value = 0
+    for c in s:
+        value = (value + ord(c)) * 17 % 256
+    return value
+
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+steps = tuple(raw.strip().split(","))
+
+print(sum(map(hashbrown, steps)))
