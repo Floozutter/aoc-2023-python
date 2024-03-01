4bad68ce496fa5ebd1eb4be775d9d36951bc03e5
Floozutter <me@floozutter.site>
2024-03-01 00:02:54 -0800

    parse day16 input

--- day16/main.py
+++ day16/main.py
@@ -2,3 +2,4 @@ INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+layout = {(i, j): c for i, line in enumerate(raw.strip().splitlines()) for j, c in enumerate(line)}
