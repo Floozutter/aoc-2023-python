a5b26eecd1ad15f825661ea6a2ea3a8629f055bd
Floozutter <me@floozutter.site>
2024-03-03 19:20:09 -0800

    parse day17 input

--- day17/main.py
+++ day17/main.py
@@ -2,3 +2,4 @@ INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+grid = {(i, j): int(c) for i, l in enumerate(raw.strip().splitlines()) for j, c in enumerate(l)}
