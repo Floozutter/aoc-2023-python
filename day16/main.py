761a96bb0bcf076fef9f8626b5dd530f93d81211
Floozutter <me@floozutter.site>
2024-03-05 17:05:50 -0800

    add day16 part 1 skeleton

--- day16/main.py
+++ day16/main.py
@@ -11,3 +11,10 @@ INPUTPATH = "input.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
 layout = {(i, j): c for i, line in enumerate(raw.strip().splitlines()) for j, c in enumerate(line)}
+
+beams = frozenset((Beam(0, 0, 0, 1),))
+energized: set[tuple[int, int]] = set()
+while beams:
+    energized.update((b.i, b.j) for b in beams)
+    beams = frozenset(y for x in beams for y in x.step(layout[x.i, x.j]) if (y.i, y.j) in layout)
+print(len(energized))
