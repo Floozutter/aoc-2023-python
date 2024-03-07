c39d0096f28a15ae82a1095ad716f6610c546227
Floozutter <me@floozutter.site>
2024-03-06 17:03:49 -0800

    refactor loop using walrus

--- day16/main.py
+++ day16/main.py
@@ -13,8 +13,7 @@ with open(INPUTPATH) as ifile:
 layout = {(i, j): c for i, line in enumerate(raw.strip().splitlines()) for j, c in enumerate(line)}
 
 beams = frozenset((Beam(0, 0, 0, 1),))
-energized: set[tuple[int, int]] = set()
-while beams:
+energized = set((b.i, b.j) for b in beams)
+while beams := frozenset(y for x in beams for y in x.step(layout[x.i, x.j]) if (y.i, y.j) in layout):
     energized.update((b.i, b.j) for b in beams)
-    beams = frozenset(y for x in beams for y in x.step(layout[x.i, x.j]) if (y.i, y.j) in layout)
 print(len(energized))
