38b0c48c5115adeba286e2f4ae228ad878cb43ac
Floozutter <me@floozutter.site>
2024-03-08 03:17:26 -0800

    avoid recreating beams

--- day16/main.py
+++ day16/main.py
@@ -17,7 +17,11 @@ with open(INPUTPATH) as ifile:
 layout = {(i, j): c for i, line in enumerate(raw.strip().splitlines()) for j, c in enumerate(line)}
 
 beams = frozenset((Beam(0, 0, 0, 1),))
+seen = set(beams)
 energized = set((b.i, b.j) for b in beams)
-while beams := frozenset(y for x in beams for y in x.step(layout[x.i, x.j]) if (y.i, y.j) in layout):
+while beams := frozenset(
+    y for x in beams for y in x.step(layout[x.i, x.j]) if (y.i, y.j) in layout and y not in seen
+):
+    seen.update(beams)
     energized.update((b.i, b.j) for b in beams)
 print(len(energized))
