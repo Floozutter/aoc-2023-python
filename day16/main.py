f9696138da75f90a44d44607d56470b02bdf2223
Floozutter <me@floozutter.site>
2024-03-09 15:58:01 -0800

    refactor into function

--- day16/main.py
+++ day16/main.py
@@ -16,18 +16,21 @@ class Beam(NamedTuple):
         else:
             return (type(self)(i+di, j+dj, di, dj),)
 
+def energized(grid: dict[tuple[int, int], str], starter: Beam) -> int:
+    beams = frozenset((starter,))
+    made = set(beams)
+    visited = set((b.i, b.j) for b in beams)
+    while beams := frozenset(
+        y for x in beams for y in x.step(grid[x.i, x.j]) if (y.i, y.j) in grid and y not in made
+    ):
+        made.update(beams)
+        visited.update((b.i, b.j) for b in beams)
+    return len(visited)
+
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
-layout = {(i, j): c for i, line in enumerate(raw.strip().splitlines()) for j, c in enumerate(line)}
+grid = {(i, j): c for i, line in enumerate(raw.strip().splitlines()) for j, c in enumerate(line)}
 
-beams = frozenset((Beam(0, 0, 0, 1),))
-seen = set(beams)
-energized = set((b.i, b.j) for b in beams)
-while beams := frozenset(
-    y for x in beams for y in x.step(layout[x.i, x.j]) if (y.i, y.j) in layout and y not in seen
-):
-    seen.update(beams)
-    energized.update((b.i, b.j) for b in beams)
-print(len(energized))
+print(energized(grid, Beam(0, 0, 0, 1)))
