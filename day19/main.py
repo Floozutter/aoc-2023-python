e3a0719c0cf02d864cf9f9987207dca4ed444c5b
Floozutter <me@floozutter.site>
2024-03-20 13:22:52 -0700

    parse parts

--- day19/main.py
+++ day19/main.py
@@ -4,4 +4,7 @@ with open(INPUTPATH) as ifile:
     raw = ifile.read()
 head, tail = raw.strip().split("\n\n")
 workflows = {}
-parts = tuple()
+parts = tuple(
+    {l: int(r) for l, r in (s.split("=") for s in line[1:-1].split(","))}
+    for line in tail.split()
+)
