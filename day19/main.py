0a7e424bde5359ecc13b0df20063c0436ab950cf
Floozutter <me@floozutter.site>
2024-03-21 13:09:39 -0700

    parse workflows

--- day19/main.py
+++ day19/main.py
@@ -3,7 +3,10 @@ INPUTPATH = "input.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
 head, tail = raw.strip().split("\n\n")
-workflows = {}
+workflows = {
+    l: tuple(r[:-1].split(","))
+    for l, r in (line.split("{") for line in head.split())
+}
 parts = tuple(
     {l: int(r) for l, r in (s.split("=") for s in line[1:-1].split(","))}
     for line in tail.split()
