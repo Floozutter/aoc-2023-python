b274741b568977d5b01b7a5376571e000f86b2fa
Floozutter <me@floozutter.site>
2024-03-24 02:44:19 -0700

    remove extra whitespace in assignment of workflows

--- day19/main.py
+++ day19/main.py
@@ -8,8 +8,5 @@ INPUTPATH = "input.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
 head, tail = raw.strip().split("\n\n")
-workflows = {
-    l: tuple(r[:-1].split(","))
-    for l, r in (line.split("{") for line in head.split())
-}
+workflows = {l: tuple(r[:-1].split(",")) for l, r in (line.split("{") for line in head.split())}
 parts = tuple(Part(s[s.index("=")+1:] for s in line[1:-1].split(","))) for line in tail.split())
